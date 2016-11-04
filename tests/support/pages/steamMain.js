069c16c9a7de5bdf27a18bc0b946de98eb1eb945
Mark Naumenko <mark_naumenkoepamcom@epbyminw1204.minsk.epam.com>
2016-11-04 11:15:47 +0300

    all fixed

--- tests/support/pages/steamMain.js
+++ tests/support/pages/steamMain.js
@@ -1,48 +1,55 @@
 var user = require('./../user'),
-	path = require('path');
+    path = require('path');
 
 var steamMain = {
-	getPage: function() {
-		return browser.get('http://store.steampowered.com/');
-	},
-	steamLogin: function() {
-		return browser.element(by.xpath('(.//*[@id="content_login"]//span[last()])[last()]')).click()
-		.then(function() {
-			browser.element(by.id('input_username')).sendKeys(user.login);
-		})
-		.then(function() {
-			browser.element(by.id('input_password')).sendKeys(user.password);	
-		})
-		.then(function() {
-			browser.element(by.xpath('.//*[@id="login_btn_signin"]/button')).click();
-		})
-		.then(function() {
-			browser.sleep(5000);
-		});
-	},
-	takeSteamScreenshot: function() {
-		var fs = require('fs'),
-			dir = path.resolve('./screenshots');
-		if (!fs.existsSync(dir)){
-    		fs.mkdirSync(dir);
-		};
-        function writeScreenShot(data, filename) {
+    getPage: function() {
+        return browser.get('http://store.steampowered.com/');
+    },
+    steamLogin: function() {
+        return browser.element(by.xpath('(.//*[@id="content_login"]//span[last()])[last()]')).click()
+        .then(function() {
+            browser.element(by.id('input_username')).sendKeys(user.login);
+        })
+        .then(function() {
+            browser.element(by.id('input_password')).sendKeys(user.password);   
+        })
+        .then(function() {
+            browser.element(by.xpath('.//*[@id="login_btn_signin"]/button')).click();
+        })
+        .then(function() {
+            browser.sleep(5000);
+        });
+    },
+    takeSteamScreenshot: function() {
+        var fs = require('fs'),
+            dir = path.resolve('./screenshots');
+
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        };
+
+        var writeScreenShot = function(data, filename) {
             var stream = fs.createWriteStream(filename);
             stream.on('open', function(fd) {
-        		stream.write(new Buffer(data, 'base64'));
-            	stream.end();
+                stream.write(new Buffer(data, 'base64'));
+                stream.end();
             });
         }
+
         return browser.takeScreenshot().then(function(png) {
-            	var currentdate = new Date(),
-            		pathname = './screenshots/shot_' + currentdate.toLocaleTimeString() + '.png',
-					file = path.resolve(pathname);
-    			fs.writeFileSync(file, png, { encoding: 'base64' }, console.log);
-  			});
+            var moment = new Date(),
+                fileDate = 
+                    moment.getFullYear() + '-' +
+                    moment.getMonth() + '-' +
+                    moment.getDate() + 'x' +
+                    moment.getHours() + '-' +
+                    moment.getMinutes() + '-' +
+                    moment.getSeconds() + 'x' +
+                    moment.getMilliseconds();
+            writeScreenShot(png, './screenshots/shot_' + fileDate + '.png');
+        });
 
-            //writeScreenShot(png, '.\\screenshots\\shot_' + currentdate.toLocaleTimeString() + '.png');
-
-	}
+    }
 };
 
 module.exports = steamMain;
